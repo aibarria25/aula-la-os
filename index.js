a01f10b29090872a65d39b977cc8605b6ec9d45c
Alejandro Ibarria Morell <141891283+aibarria25@users.noreply.github.com>
2023-09-04 21:47:50 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+Exercícios de interpretação de código
+
+1- 
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)
+Vai somar os numeros de 0 ate 4 (0 + 1 + 2 + 3 + 4 = 10)
+
+2- 
+const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}
+
+a) No console vai ser impresso os numeros que sejam maior do que 18 (19, 21, 23, 25, 27, 30)
+*b)
+
+3- No console vai ser impresso 4 linhas de asteriscos e vai adicionar um astericos em cada linha ate chegar a 4
+*
+**
+***
+****
+
+*/ 
+//xercícios de escrita de código
+//1- 
+
+
